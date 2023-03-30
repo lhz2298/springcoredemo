306b33d60929c7df413ac48d9b42d73369f38d54
Michael Li <xinghangli@Xinghangs-MacBook-Pro-2.local>
2023-03-29 20:43:35 -0700

    Finished Constructor Injection

---
+++ src/main/java/com/luv2code/springcoredemo/DemoRestController.java
@@ -0,0 +1,23 @@
+package com.luv2code.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoRestController {
+
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    // define a constructor for dependency injection
+    @Autowired
+    public DemoRestController(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
