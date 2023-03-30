edfb947efbcb543ebdaa7e838c1bc15e36d1c749
Michael Li <xinghangli@Xinghangs-MacBook-Pro-2.local>
2023-03-29 21:32:35 -0700

    Finished Qualifiers

--- src/main/java/com/luv2code/springcoredemo/rest/DemoRestController.java
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoRestController.java
@@ -2,6 +2,7 @@ package com.luv2code.springcoredemo.rest;
 
 import com.luv2code.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +12,13 @@ public class DemoRestController {
     // define a private field for the dependency
     private Coach myCoach;
 
+    public DemoRestController(@Qualifier("cricketCoach") Coach theCoach) {
+        this.myCoach = theCoach;
+    }
+
     // define a setter for dependency injection
     @Autowired
-    public void setCoach(Coach theCoach) {
-        myCoach = theCoach;
-    }
+
 
     @GetMapping("/dailyworkout")
     String getDailyWorkout() {
